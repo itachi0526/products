c4417fcfd849bb1800af1e6e4fe1efed3765f003
Eric Haung <fiser0317@gmail.com>
2021-06-09 21:28:57 +0800

    add function

--- products.py
+++ products.py
@@ -1,38 +1,51 @@
 import os # operating system
 
-products = [] # 寫在最初是為了無論找不找的到檔案，都要執行
-if os.path.isfile('products.csv'):  # 作業系統中的模組path中其中一個功能isfile，檢查檔案在不在
-    print('找到檔案了！') # 找完檔案要開始讀取檔案！！
-    with open('products.csv', 'r', encoding = 'utf-8') as f:
+# 讀取檔案
+def read_file(filename): # 檔名給filename，不要寫死
+    products = [] # 寫在最初是為了無論找不找的到檔案，都要執行
+    with open(filename, 'r', encoding='utf-8') as f:
         for line in f:
             if '商品,價格' in line:
                 continue # 跳過它，繼續
             s = line.strip().split(',') # split變成一塊一塊的東西，s為清單
             name = s[0]
             price = s[1]
-            # 6～7進階，直接把name, price取代4的s
+            # 6～7進階，直接把name, price取代8的s
             products.append([name, price])
-    print(products)
-
-else:
-    print('找不到檔案Q_Q')
+    return products     
 
 # 讓使用者輸入
-while True:
-    name = input('請輸入商品名稱： ')
-    if name == 'q':
-        break
-    price = input('請輸入商品價格： ')
-    price = int(price)
-    products.append([name, price])
-print(products)
+def user_input(products):
+    while True:
+        name = input('請輸入商品名稱： ')
+        if name == 'q':
+            break
+        price = input('請輸入商品價格： ')
+        price = int(price)
+        products.append([name, price])
+    print(products)
+    return products
 
 # 印出所有購買紀錄
-for p in products:
-    print(p[0], '的價格', p[1])
-
-# 寫入檔案
-with open('products.csv', 'w', encoding = 'utf-8') as f:
-    f.write('商品,價格\n')
+def print_products(products):
     for p in products:
-        f.write(p[0] + ',' + str(p[1]) + '\n')    
+        print(p[0], '的價格', p[1])
+
+def write_file(filename, products):
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write('商品,價格\n')
+        for p in products:
+            f.write(p[0] + ',' + str(p[1]) + '\n')
+
+def main():
+    filename = 'products.csv'
+    if os.path.isfile(filename):  # 作業系統中的模組path中其中一個功能isfile，檢查檔案在不在
+        print('找到檔案了')
+        products = read_file(filename)
+    else:
+        print('找不到檔案Q_Q')    
+    products = user_input(products)
+    print_products(products)
+    write_file('products.csv', products)
+
+main()    
